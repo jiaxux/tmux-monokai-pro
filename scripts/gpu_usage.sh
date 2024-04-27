e006312d335a3c2125c70a084a9223b7c605acce
Jiaxu Xing <49942794+jiaxux@users.noreply.github.com>
2024-04-27 11:47:24 +0200

    Update gpu_usage.sh

--- scripts/gpu_usage.sh
+++ scripts/gpu_usage.sh
@@ -9,7 +9,8 @@ get_platform()
 {
   case $(uname -s) in
     Linux)
-      gpu=$(lspci -v | grep VGA | head -n 1 | awk '{print $5}')
+      gpu=$(lspci -v | grep VGA | tail -n 1 | awk '{print $5}')
+      
       echo $gpu
       ;;
 
